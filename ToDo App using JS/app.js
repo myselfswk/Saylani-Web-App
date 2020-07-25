ec4c080e0a80e24eb5663142597206f34d68c588
Muhammad Waleed Khan <41314353+myselfswk@users.noreply.github.com>
2020-07-25 23:44:08 +0500

    Todo App
    ToDoApp using HTML,CSS and JavaScript

---
+++ ToDo App using JS/app.js
@@ -0,0 +1,48 @@
+var list = document.getElementById("list");
+
+function addTodo() {
+    var todo_item = document.getElementById("todo-item");
+
+
+    // create li tag with text node
+    if (todo_item.value !==""){
+        var li = document.createElement('li')
+        var liText = document.createTextNode(todo_item.value)
+        li.appendChild(liText)
+
+        // create delete button
+        var delBtn = document.createElement("button")
+        var delText = document.createTextNode("DELETE")
+        delBtn.setAttribute("class", "btn")
+        delBtn.setAttribute("onclick", "deleteItem(this)")
+        delBtn.appendChild(delText)
+
+        // create edit button
+        var editBtn = document.createElement("button");
+        var editText = document.createTextNode("EDIT")
+        editBtn.appendChild(editText)
+        editBtn.setAttribute("onclick", "editItem(this)")
+        editBtn.setAttribute("class", "btnedit")
+
+
+        li.appendChild(delBtn)
+        li.appendChild(editBtn)
+
+        list.appendChild(li)
+
+        todo_item.value = "";
+    }
+}
+
+function deleteItem(e) {
+    e.parentNode.remove()
+}
+
+function editItem(e) {
+  var val = prompt("Enter updated value",e.parentNode.firstChild.nodeValue);
+  e.parentNode.firstChild.nodeValue = val;
+}
+
+function deleteAll() {
+    list.innerHTML = ""
+}
